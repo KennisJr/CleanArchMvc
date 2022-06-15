234b4655455479da966e7f5c4637957d2133c860
Edson Kennis <Edson.K.Santos@outlook.com>
2022-06-15 09:59:36 -0300

    Domain getting rich

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,33 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public sealed class Category : Entity
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public Category(String name)
+        {
+            ValidateDomain(name);
+        }
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            ValidateDomain(name);
+
+        }
+        public string Name { get; private set; }
 
         public ICollection<Product> Products { get; set; }
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name,Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 3 charecters");
+            Name = name;
+        }
     }
 }
---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Category> GetById(int? id);
+
+        Task<Product> GetProductCategory( int? id);
+        Task<Category> Create(Product product);
+        Task<Category> Update(Product product);
+        Task<Category> Remove(Product product);
+    }
+}

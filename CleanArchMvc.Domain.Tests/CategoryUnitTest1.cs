b0f182c59771c4ba0a13b28a1a5b36036ddbdf3a
Edson Kennis <Edson.K.Santos@outlook.com>
2022-06-15 10:15:24 -0300

    xUnit, Category tests

--- CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
+++ CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
@@ -14,5 +14,41 @@ namespace CleanArchMvc.Domain.Tests
             action.Should()
                 .NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
         }
+
+        [Fact]
+        public void CreateCategory_WithNegativeValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Category(-1, "Category Name");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value");
+        }
+
+        [Fact]
+        public void CreateCategory_MissingNameValue_DomainExceptionRequiredName()
+        {
+            Action action = () => new Category(1, "");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name,Name is required");
+        }
+
+        [Fact]
+        public void CreateCategory_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Category(1, "XU");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short, minimum 3 charecters");
+        }
+
+        [Fact]
+        public void CreateCategory_WithNullValue_DomainExceptionInvalidName()
+        {
+            Action action = () => new Category(1, null);
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name,Name is required");
+        }
     }
 }

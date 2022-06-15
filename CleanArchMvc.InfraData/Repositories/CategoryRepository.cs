efa74217cb0221479b4d89971569c09285be229e
Edson Kennis <Edson.K.Santos@outlook.com>
2022-06-15 13:58:43 -0300

    Make DbContext and DbEntityConfig

---
+++ CleanArchMvc.InfraData/Repositories/CategoryRepository.cs
@@ -0,0 +1,47 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.InfraData.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.InfraData.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        ApplicationDbContext _categoryContext;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+           _categoryContext = context;
+        }
+
+        public async Task<Category> Create(Category category)
+        {
+            _categoryContext.Add(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> GetById(int? id)
+        {
+            
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Category> Remove(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Category> Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
